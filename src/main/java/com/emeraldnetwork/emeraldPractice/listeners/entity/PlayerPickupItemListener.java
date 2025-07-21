ea4ff77da9ad1d6c05277f7e2ffcb3af2a70787b
AvisDylan <avisdylanmc@gmail.com>
2025-07-21 17:22:16 +0100

    Add PlayerPickupItemListener to handle item pickup restrictions

---
+++ src/main/java/com/emeraldnetwork/emeraldPractice/listeners/entity/PlayerPickupItemListener.java
@@ -0,0 +1,23 @@
+/**
+ * Created by dylan on 7/21/2025
+ */
+
+package com.emeraldnetwork.emeraldPractice.listeners.entity;
+
+import com.emeraldnetwork.emeraldPractice.player.PlayerData;
+import com.emeraldnetwork.emeraldPractice.player.PlayerManager;
+import com.emeraldnetwork.emeraldPractice.player.PlayerState;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+public class PlayerPickupItemListener implements Listener{
+    
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupItemEvent event){
+        PlayerData playerData = PlayerManager.getPlayerData(event.getPlayer().getUniqueId());
+        
+        if(playerData.getPlayerState() != PlayerState.DUEL || playerData.getPlayerState() != PlayerState.FFA)
+            event.setCancelled(true);
+    }
+}
